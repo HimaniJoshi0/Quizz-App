c6955d2b80d59f8653ad7eec403795b53075216e
Himanijoshi0 <himanijoshi225@gmail.com>
2025-05-24 11:26:48 +0530

    quiz marks

--- src/pages/quiztest/index.tsx
+++ src/pages/quiztest/index.tsx
@@ -5,6 +5,7 @@ const Quiztest = () => {
   const { quizzData } = useStore()
   const [showResults, setSHowResults] = useState(false)
   const [answers, setAnswers] = useState<{ [questionId: number]: number | undefined }>({})
+  const [totalMarks, setTotalMarks] = useState(0)
 
   const handleOptionChange = (questionId: number, optionId: number) => {
     setAnswers((prev) => ({
@@ -21,39 +22,49 @@ const Quiztest = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        {quizzData?.topic}
-        {quizzData?.questions.map((question, idx) => (
-          <div key={question.id} style={{ marginBottom: '1.5rem' }}>
-            <h3>{`Q${idx + 1}. ${question.text}`}</h3>
-            <ul>
-              {question.options.map((option) => (
-                <li key={option.id} 
-                >
-                  <label className={showResults? option.isCorrect? "border border-green-500": answers[question.id] === option.id ? "border border-red-500" : "" : ""}>
-                    <input
-                      type="radio"
-                      name={`question-${question.id}`}
-                      value={option.id}
-                      checked={answers[question.id] === option.id}
-                      onChange={() => handleOptionChange(question.id, option.id)}
-                    />
-                    {option.text}
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-        <button
-          type="submit"
-          className="mt-4 bg-blue-600 text-white px-4 py-2 rounded"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+    <>
+      {
+        showResults ?
+          <p>{totalMarks}</p> : <></>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          {quizzData?.topic}
+          {quizzData?.questions.map((question, idx) => (
+            <div key={question.id} style={{ marginBottom: '1.5rem' }}>
+              <h3>{`Q${idx + 1}. ${question.text}`}</h3>
+              <ul>
+                {question.options.map((option) => (
+                  <li key={option.id}
+                  >
+                    <label className={showResults ? option.isCorrect ? "border border-green-500" : answers[question.id] === option.id ? "border border-red-500" : "" : ""}>
+                      <input
+                        type="radio"
+                        name={`question-${question.id}`}
+                        value={option.id}
+                        checked={answers[question.id] === option.id}
+                        onChange={() => {
+                          handleOptionChange(question.id, option.id);
+                          if (option.isCorrect) {
+                            setTotalMarks((prev) => prev + 1)
+                          }
+                        }}
+                      />
+                      {option.text}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <button
+            type="submit"
+            className="mt-4 bg-blue-600 text-white px-4 py-2 rounded"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
 
